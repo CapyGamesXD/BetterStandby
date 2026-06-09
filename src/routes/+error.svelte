2758c153dd00210f295231aa9eabb6f6c45cd182
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-06-09 17:38:25 +0100

    Updated 404 page.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="centerdiv">
+	<img src="/capybara.png" alt="A bored sitting capybara" />
+	<h1>{page.error?.message}!</h1>
+	<div class="divider mb-2"></div>
+	<p>Please check that the link is correct. Otherwise continue staring at Jeffrey the capybara.</p>
+	<p class="mb-5">If you think this is an issue, please contact Capy (me) on Slack. :D</p>
+</div>
